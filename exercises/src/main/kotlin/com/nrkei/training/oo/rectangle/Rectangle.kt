4144eeedaa6b8d809071711a95ef66fcbdd48a21
Fred George <fredgeorge@acm.org>
2022-03-14 10:08:18 +0100

    Fred: Rectangle with area(), perimeter() and range checks on construction

--- exercises/src/main/kotlin/com/nrkei/training/oo/rectangle/Rectangle.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/rectangle/Rectangle.kt
@@ -6,7 +6,21 @@
 
 package com.nrkei.training.oo.rectangle
 
-// JOB_GOES_HERE
-class Rectangle {
+// Understands a four-sided polygon with sides at right angles
+class Rectangle(length: Number, width: Number) {
+    private val length = length.toDouble()
+    private val width = width.toDouble()
 
+    init {
+        require(this.length > 0.0 && this.width > 0.0)
+        { "Each Rectangle dimension must be greater than zero" }
+    }
+
+    fun area() = length * width
+
+    val area get() = area()
+
+    fun perimeter() = 2 * (length + width)
+
+    val perimeter get() = perimeter()
 }
--- tests/src/test/kotlin/com/nrkei/training/oo/unit/RectangleTest.kt
+++ tests/src/test/kotlin/com/nrkei/training/oo/unit/RectangleTest.kt
@@ -9,11 +9,25 @@ package com.nrkei.training.oo.unit
 import com.nrkei.training.oo.rectangle.Rectangle
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 // Ensures Rectangle operates correctly
 internal class RectangleTest {
 
     @Test fun area() {
         assertEquals(24.0, Rectangle(4.0, 6.0).area())
+        assertEquals(24.0, Rectangle(4, 6).area())
+        assertEquals(24.0, Rectangle(4, 6).area)
+    }
+
+    @Test fun perimeter() {
+        assertEquals(20.0, Rectangle(4.0, 6.0).perimeter())
+        assertEquals(20.0, Rectangle(4, 6).perimeter())
+        assertEquals(20.0, Rectangle(4, 6).perimeter)
+    }
+
+    @Test fun `range checks`() {
+        assertThrows<IllegalArgumentException> { Rectangle(0, 6.0) }
+        assertThrows<IllegalArgumentException> { Rectangle(4.0, 0) }
     }
 }
