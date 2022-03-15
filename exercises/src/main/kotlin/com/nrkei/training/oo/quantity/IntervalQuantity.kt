d93b48d03b27bf56d8717e8cc248068419422203
Fred George <fredgeorge@acm.org>
2022-03-15 16:07:06 +0100

    Fred: Finding the largest Quantity (volume, distance, temperature)

--- exercises/src/main/kotlin/com/nrkei/training/oo/quantity/IntervalQuantity.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/quantity/IntervalQuantity.kt
@@ -6,10 +6,11 @@
 
 package com.nrkei.training.oo.quantity
 
+import com.nrkei.training.oo.order.Orderable
 import com.nrkei.training.oo.quantity.Unit.Companion.EPSILON
 import kotlin.math.absoluteValue
 
-open class IntervalQuantity(amount: Number, protected val unit: Unit) {
+open class IntervalQuantity(amount: Number, protected val unit: Unit) : Orderable<IntervalQuantity> {
     protected val amount = amount.toDouble()
 
     override fun equals(other: Any?) = this === other || other is IntervalQuantity && this.equals(other)
@@ -22,4 +23,6 @@ open class IntervalQuantity(amount: Number, protected val unit: Unit) {
     protected fun convertedAmount(other: IntervalQuantity) = this.unit.convertedAmount(other.amount, other.unit)
 
     override fun hashCode() = unit.hashCode(amount)
+
+    override fun isBetterThan(other: IntervalQuantity) = this.amount > convertedAmount(other)
 }
--- tests/src/test/kotlin/com/nrkei/training/oo/unit/OrderableTest.kt
+++ tests/src/test/kotlin/com/nrkei/training/oo/unit/OrderableTest.kt
@@ -8,6 +8,13 @@ package com.nrkei.training.oo.unit
 
 import com.nrkei.training.oo.order.bestOrNull
 import com.nrkei.training.oo.probability.Chance
+import com.nrkei.training.oo.quantity.RatioQuantity
+import com.nrkei.training.oo.quantity.Unit.Companion.celsius
+import com.nrkei.training.oo.quantity.Unit.Companion.cups
+import com.nrkei.training.oo.quantity.Unit.Companion.fahrenheit
+import com.nrkei.training.oo.quantity.Unit.Companion.gallons
+import com.nrkei.training.oo.quantity.Unit.Companion.ounces
+import com.nrkei.training.oo.quantity.Unit.Companion.quarts
 import com.nrkei.training.oo.rectangle.Rectangle
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
@@ -23,5 +30,13 @@ internal class OrderableTest {
     @Test internal fun `least likely chance`() {
         assertEquals(Chance(0), listOf(Chance(0.1), Chance(0.7), Chance(0), Chance(0.9)).bestOrNull())
         assertNull(emptyList<Chance>().bestOrNull())
+    }
+
+    @Test fun `largest Quantity`() {
+        assertEquals(2.quarts, listOf(
+            0.2.gallons, 24.ounces, 0.5.gallons, 7.cups).bestOrNull())
+        assertNull(emptyList<RatioQuantity>().bestOrNull())
+        assertEquals(100.celsius, listOf(
+            212.fahrenheit, 0.celsius, 50.fahrenheit, (-40).celsius).bestOrNull())
     }
 }
