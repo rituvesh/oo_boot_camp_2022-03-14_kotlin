07c801c55988c8087a18bc5c8cbbed8df81d4ac2
Fred George <fredgeorge@acm.org>
2022-03-16 10:39:19 +0100

    Fred: Graph with hopCount(); not necessarily minimum; refactoring possible

--- exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
@@ -6,12 +6,32 @@
 
 package com.nrkei.training.oo.graph
 
-class Node{
+class Node {
+    companion object {
+        private const val UNREACHABLE = -1
+    }
+
     private val neighbors = mutableListOf<Node>()
 
     infix fun to(neighbor: Node) = neighbor.also { neighbors.add(neighbor) }
 
     infix fun canReach(destination: Node) = this.canReach(destination, noVisitedNodes)
+
+    infix fun hopCount(destination: Node) = this.hopCount(destination, noVisitedNodes).also {
+        require(it != UNREACHABLE) { "Destination cannot be reached" }
+    }
+
+    private fun hopCount(destination: Node, visitedNodes: MutableList<Node>): Int {
+        if (this == destination) return 0
+        if (this in visitedNodes) return UNREACHABLE
+        visitedNodes.add(this)
+        neighbors.forEach { neighbor ->
+            neighbor.hopCount(destination, visitedNodes).also { neighborHopCount ->
+                if (neighborHopCount != UNREACHABLE) return neighborHopCount + 1
+            }
+        }
+        return UNREACHABLE
+    }
 
     private fun canReach(destination: Node, visitedNodes: MutableList<Node>): Boolean {
         if (this == destination) return true
--- tests/src/test/kotlin/com/nrkei/training/oo/unit/GraphTest.kt
+++ tests/src/test/kotlin/com/nrkei/training/oo/unit/GraphTest.kt
@@ -7,9 +7,9 @@
 package com.nrkei.training.oo.unit
 
 import com.nrkei.training.oo.graph.Node
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class GraphTest {
     companion object {
@@ -38,5 +38,16 @@ internal class GraphTest {
         assertFalse(G canReach B)
         assertFalse(A canReach B)
         assertFalse(B canReach G)
+    }
+
+    @Test fun `hop count`() {
+        assertEquals(0, B hopCount B)
+        assertEquals(1, B hopCount A)
+        assertEquals(1, B hopCount F)
+        assertEquals(2, B hopCount D)
+        assertEquals(4, C hopCount F)
+        assertThrows<IllegalArgumentException>{ G hopCount B }
+        assertThrows<IllegalArgumentException>{ A hopCount B }
+        assertThrows<IllegalArgumentException>{ B hopCount G }
     }
 }
