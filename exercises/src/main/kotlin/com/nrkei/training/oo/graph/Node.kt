0b0f75fcad8f9fc934b78fa496a50eac526a81ef
Fred George <fredgeorge@acm.org>
2022-03-16 10:40:49 +0100

    Fred: Graph with hopCount(); not necessarily minimum; canReach() uses hopCount()

--- exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
@@ -15,7 +15,7 @@ class Node {
 
     infix fun to(neighbor: Node) = neighbor.also { neighbors.add(neighbor) }
 
-    infix fun canReach(destination: Node) = this.canReach(destination, noVisitedNodes)
+    infix fun canReach(destination: Node) = this.hopCount(destination, noVisitedNodes) != UNREACHABLE
 
     infix fun hopCount(destination: Node) = this.hopCount(destination, noVisitedNodes).also {
         require(it != UNREACHABLE) { "Destination cannot be reached" }
@@ -31,13 +31,6 @@ class Node {
             }
         }
         return UNREACHABLE
-    }
-
-    private fun canReach(destination: Node, visitedNodes: MutableList<Node>): Boolean {
-        if (this == destination) return true
-        if (this in visitedNodes) return false
-        visitedNodes.add(this)
-        return neighbors.any { it.canReach(destination, visitedNodes) }
     }
 
     private val noVisitedNodes get() = mutableListOf<Node>()
